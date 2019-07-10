aa16361030782ce7e3e4ed13240402a0d701f326
Jean Georges Perrin <jgp@jgp.net>
2019-07-10 15:41:17 -0400

    Update

---
+++ src/main/java/net/jgp/books/sparkWithJava/ch20/lab100/standaloneDataframe/StandaloneDataframeApp.java
@@ -0,0 +1,36 @@
+package net.jgp.books.sparkWithJava.ch20.lab100.standaloneDataframe;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * .
+ * 
+ * @author jgp
+ *
+ */
+public class StandaloneDataframeApp {
+
+  public static void main(String[] args) {
+    StandaloneDataframeApp app = new StandaloneDataframeApp();
+    app.start();
+  }
+
+  private void start() {
+    SparkSession spark = SparkSession.builder()
+        .appName("Standalone dataframe")
+        //.master("spark://172.31.35.171:7077")
+        .getOrCreate();
+
+    String[] l = new String[] { "a", "b", "c", "d" };
+    List<String> data = Arrays.asList(l);
+    Dataset<String> ds = spark.createDataset(data, Encoders.STRING());
+    Dataset<Row> df = ds.toDF();
+    df.show();
+  }
+}
