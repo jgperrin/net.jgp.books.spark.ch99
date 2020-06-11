36ffe3c53a6cb8ed884e2f401d3bdae06235b364
jgp <jgp@jgp.net>
2020-06-11 15:11:47 -0400

    clean up data

--- src/main/java/net/jgp/books/spark/ch99/covid19/lab100_ingestion/SimpleDataIngestionApp.java
+++ src/main/java/net/jgp/books/spark/ch99/covid19/lab100_ingestion/SimpleDataIngestionApp.java
@@ -1,20 +1,9 @@
 package net.jgp.books.spark.ch99.covid19.lab100_ingestion;
 
-import static org.apache.spark.sql.functions.callUDF;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.apache.spark.ml.feature.VectorAssembler;
-import org.apache.spark.ml.linalg.Vector;
-import org.apache.spark.ml.linalg.Vectors;
-import org.apache.spark.ml.regression.LinearRegression;
-import org.apache.spark.ml.regression.LinearRegressionModel;
-import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-
-import net.jgp.books.spark.ch99.dq.lab200_dq_ml.dq.udf.MinimumPriceDataQualityUdf;
-import net.jgp.books.spark.ch99.dq.lab200_dq_ml.dq.udf.PriceCorrelationDataQualityUdf;
+import net.jgp.books.spark.ch99.covid19.x.utils.GitUtils;
 
 /**
  * Cleans a dataset and then extrapolates date through machine learning, via
@@ -24,6 +13,8 @@ import net.jgp.books.spark.ch99.dq.lab200_dq_ml.dq.udf.PriceCorrelationDataQuali
  *
  */
 public class SimpleDataIngestionApp {
+  private static Logger log =
+      LoggerFactory.getLogger(SimpleDataIngestionApp.class);
 
   public static void main(String[] args) {
     SimpleDataIngestionApp app =
@@ -35,125 +26,136 @@ public class SimpleDataIngestionApp {
    * Real work goes here...
    */
   private void start() {
-    SparkSession spark = SparkSession.builder()
-        .appName("DQ4ML")
-        .master("local[*]")
-        .getOrCreate();
+    log.debug("-> start()");
 
-    // DQ Section
-    // ----------
+    // Clone
+    GitUtils.syncRepository(
+        "https://github.com/CSSEGISandData/COVID-19.git",
+        "./data/covid19-jhu");
 
-    spark.udf().register("minimumPriceRule",
-        new MinimumPriceDataQualityUdf(), DataTypes.DoubleType);
-    spark.udf().register("priceCorrelationRule",
-        new PriceCorrelationDataQualityUdf(), DataTypes.DoubleType);
-
-    // Load our dataset
-    String filename = "data/restaurant/checks.csv";
-    Dataset<Row> df = spark
-        .read()
-        .format("csv")
-        .option("inferSchema", true)
-        .option("header", false)
-        .load(filename);
-
-    // simple renaming of the columns
-    df = df.withColumnRenamed("_c0", "guest");
-    df = df.withColumnRenamed("_c1", "price");
-
-    System.out.println("----");
-    System.out.println("Load & Format");
-    df.show();
-    System.out.println("----");
-
-    // apply DQ rules
-    // 1) min price
-    df = df.withColumn("price_no_min",
-        callUDF("minimumPriceRule", df.col("price")));
-    System.out.println("----");
-    System.out.println("1st DQ rule");
-    df.printSchema();
-    df.show(50);
-    System.out.println("----");
-
-    df.createOrReplaceTempView("price");
-    df = spark.sql(
-        "SELECT cast(guest as int) guest, price_no_min AS price FROM price WHERE price_no_min > 0");
-    System.out.println("----");
-    System.out.println("1st DQ rule - clean-up");
-    df.printSchema();
-    df.show(50);
-    System.out.println("----");
-
-    // 2) correlated price
-    df = df.withColumn("price_correct_correl",
-        callUDF("priceCorrelationRule", df.col("price"), df.col("guest")));
-    df.createOrReplaceTempView("price");
-    df = spark.sql(
-        "SELECT guest, price_correct_correl AS price FROM price WHERE price_correct_correl > 0");
-
-    System.out.println("----");
-    System.out.println("2nd DQ rule");
-    df.show(50);
-    System.out.println("----");
-
-    // ML Section
-    // ----------
-
-    // Creates the "label" column, required by the LR algorithm.
-    df = df.withColumn("label", df.col("price"));
-
-    // Puts all the columns that will be part of the feature in an array, so
-    // you can assemble them later. Here we have only one column in our
-    // feature.
-    String[] inputCols = new String[1];
-    inputCols[0] = "guest";
-
-    // Assembles the features in one column called "features".
-    VectorAssembler assembler = new VectorAssembler()
-        .setInputCols(inputCols)
-        .setOutputCol("features");
-    df = assembler.transform(df);
-    df.printSchema();
-    df.show();
-
-    // Lots of complex ML code goes here (just kidding...)
-
-    // Build the linear regression
-    LinearRegression lr = new LinearRegression()
-        .setMaxIter(40)
-        .setRegParam(1)
-        .setElasticNetParam(1);
-
-    // Fit the model to the data
-    LinearRegressionModel model = lr.fit(df);
-
-    // Given a dataset, predict each point's label, and show the results.
-    model.transform(df).show();
-
-    // Mostly debug and info-to-look-smart
-    LinearRegressionTrainingSummary trainingSummary = model.summary();
-    System.out
-        .println("numIterations: " + trainingSummary.totalIterations());
-    System.out.println("objectiveHistory: "
-        + Vectors.dense(trainingSummary.objectiveHistory()));
-    trainingSummary.residuals().show();
-    System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
-    System.out.println("r2: " + trainingSummary.r2());
-
-    double intersect = model.intercept();
-    System.out.println("Intersection: " + intersect);
-    double regParam = model.getRegParam();
-    System.out.println("Regression parameter: " + regParam);
-    double tol = model.getTol();
-    System.out.println("Tol: " + tol);
-
-    // Prediction code
-    Double feature = 40.0;
-    Vector features = Vectors.dense(40.0);
-    double p = model.predict(features);
-
-    // Catering business outcome for 40 guests
-    System.out.println("Prediction for " + feature + " guests is " + p);
+    // SparkSession spark = SparkSession.builder()
+    // .appName("DQ4ML")
+    // .master("local[*]")
+    // .getOrCreate();
+    //
+    // // DQ Section
+    // // ----------
+    //
+    // spark.udf().register("minimumPriceRule",
+    // new MinimumPriceDataQualityUdf(), DataTypes.DoubleType);
+    // spark.udf().register("priceCorrelationRule",
+    // new PriceCorrelationDataQualityUdf(), DataTypes.DoubleType);
+    //
+    // // Load our dataset
+    // String filename = "data/restaurant/checks.csv";
+    // Dataset<Row> df = spark
+    // .read()
+    // .format("csv")
+    // .option("inferSchema", true)
+    // .option("header", false)
+    // .load(filename);
+    //
+    // // simple renaming of the columns
+    // df = df.withColumnRenamed("_c0", "guest");
+    // df = df.withColumnRenamed("_c1", "price");
+    //
+    // System.out.println("----");
+    // System.out.println("Load & Format");
+    // df.show();
+    // System.out.println("----");
+    //
+    // // apply DQ rules
+    // // 1) min price
+    // df = df.withColumn("price_no_min",
+    // callUDF("minimumPriceRule", df.col("price")));
+    // System.out.println("----");
+    // System.out.println("1st DQ rule");
+    // df.printSchema();
+    // df.show(50);
+    // System.out.println("----");
+    //
+    // df.createOrReplaceTempView("price");
+    // df = spark.sql(
+    // "SELECT cast(guest as int) guest, price_no_min AS price FROM price
+    // WHERE price_no_min > 0");
+    // System.out.println("----");
+    // System.out.println("1st DQ rule - clean-up");
+    // df.printSchema();
+    // df.show(50);
+    // System.out.println("----");
+    //
+    // // 2) correlated price
+    // df = df.withColumn("price_correct_correl",
+    // callUDF("priceCorrelationRule", df.col("price"), df.col("guest")));
+    // df.createOrReplaceTempView("price");
+    // df = spark.sql(
+    // "SELECT guest, price_correct_correl AS price FROM price WHERE
+    // price_correct_correl > 0");
+    //
+    // System.out.println("----");
+    // System.out.println("2nd DQ rule");
+    // df.show(50);
+    // System.out.println("----");
+    //
+    // // ML Section
+    // // ----------
+    //
+    // // Creates the "label" column, required by the LR algorithm.
+    // df = df.withColumn("label", df.col("price"));
+    //
+    // // Puts all the columns that will be part of the feature in an array,
+    // so
+    // // you can assemble them later. Here we have only one column in our
+    // // feature.
+    // String[] inputCols = new String[1];
+    // inputCols[0] = "guest";
+    //
+    // // Assembles the features in one column called "features".
+    // VectorAssembler assembler = new VectorAssembler()
+    // .setInputCols(inputCols)
+    // .setOutputCol("features");
+    // df = assembler.transform(df);
+    // df.printSchema();
+    // df.show();
+    //
+    // // Lots of complex ML code goes here (just kidding...)
+    //
+    // // Build the linear regression
+    // LinearRegression lr = new LinearRegression()
+    // .setMaxIter(40)
+    // .setRegParam(1)
+    // .setElasticNetParam(1);
+    //
+    // // Fit the model to the data
+    // LinearRegressionModel model = lr.fit(df);
+    //
+    // // Given a dataset, predict each point's label, and show the results.
+    // model.transform(df).show();
+    //
+    // // Mostly debug and info-to-look-smart
+    // LinearRegressionTrainingSummary trainingSummary = model.summary();
+    // System.out
+    // .println("numIterations: " + trainingSummary.totalIterations());
+    // System.out.println("objectiveHistory: "
+    // + Vectors.dense(trainingSummary.objectiveHistory()));
+    // trainingSummary.residuals().show();
+    // System.out.println("RMSE: " +
+    // trainingSummary.rootMeanSquaredError());
+    // System.out.println("r2: " + trainingSummary.r2());
+    //
+    // double intersect = model.intercept();
+    // System.out.println("Intersection: " + intersect);
+    // double regParam = model.getRegParam();
+    // System.out.println("Regression parameter: " + regParam);
+    // double tol = model.getTol();
+    // System.out.println("Tol: " + tol);
+    //
+    // // Prediction code
+    // Double feature = 40.0;
+    // Vector features = Vectors.dense(40.0);
+    // double p = model.predict(features);
+    //
+    // // Catering business outcome for 40 guests
+    // System.out.println("Prediction for " + feature + " guests is " + p);
   }
 }
---
+++ src/main/java/net/jgp/books/spark/ch99/covid19/x/utils/GitUtils.java
@@ -0,0 +1,68 @@
+package net.jgp.books.spark.ch99.covid19.x.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class GitUtils {
+  private static Logger log =
+      LoggerFactory.getLogger(GitUtils.class);
+
+  /**
+   * Syncs (commits or pulls) a repo.
+   * 
+   * @param repositoryUrl
+   * @param destinationPath
+   * @return
+   */
+  public static boolean syncRepository(String repositoryUrl,
+      String destinationPath) {
+    log.debug("-> syncRepository()");
+
+    Git git;
+    File dest = new File(destinationPath);
+
+    if (dest.exists()) {
+      // pull
+      Repository localRepo;
+      try {
+        localRepo = new FileRepository(destinationPath + "/.git");
+      } catch (IOException e) {
+        log.error("Exception reading a Git repo at {}, got {}.",
+            destinationPath, e.getMessage());
+        return false;
+      }
+      git = new Git(localRepo);
+      PullCommand pullCmd = git.pull();
+      try {
+        pullCmd.call();
+      } catch (GitAPIException e) {
+        e.printStackTrace();
+        return false;
+      }
+    } else {
+      // clone
+      try {
+        git = Git.cloneRepository()
+            .setURI(repositoryUrl)
+            .setDirectory(dest)
+            .call();
+      } catch (GitAPIException e) {
+        log.error("Exception while creating a local Git repo at {}, got {}.",
+            repositoryUrl, e.getMessage());
+        return false;
+      }
+    }
+
+    log.debug("<- syncRepository() - ok");
+    return true;
+  }
+
+}
