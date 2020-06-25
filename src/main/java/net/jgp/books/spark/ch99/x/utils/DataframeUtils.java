fc6f34e45a5be286b0859c25c505afc881fbd3e7
jgp <jgp@jgp.net>
2020-06-25 16:50:22 -0400

    Stats on columns

--- src/main/java/net/jgp/books/spark/ch99/x/utils/DataframeUtils.java
+++ src/main/java/net/jgp/books/spark/ch99/x/utils/DataframeUtils.java
@@ -2,13 +2,22 @@ package net.jgp.books.spark.ch99.x.utils;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import static org.apache.spark.sql.functions.*;
 
 public abstract class DataframeUtils {
 
   public static void show(Dataset<Row> df) {
-    df.sample(0.99).show(100, false);
+    df.sample(1).show(100, false);
     df.printSchema();
-    System.out.println(df.count() + " records.");
+    System.out.println(df.rdd().getNumPartitions() + " partition(s).");
+    System.out.println(df.count() + " record(s).");
+  }
+
+  public static void analyzeColumn(Dataset<Row> df, String col) {
+    Dataset<Row> workDf = df.groupBy(col).agg(count(col).as(K.COUNT)).orderBy(col(K.COUNT).desc());
+    workDf.show();
+    workDf.orderBy(col(K.COUNT).asc()).show(5);
+    workDf.printSchema();
   }
 
 }
